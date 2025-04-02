b1a319baccfef03f5b8fb7b2e4c08aaa6ef00d47
bmp-dot <bmpeterson@gmail.com>
2025-04-01 21:43:11 -0600

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"], // 👈 tell Tailwind where to look
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
