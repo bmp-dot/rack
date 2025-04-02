8232a93d4793d4ffd7304b8e855973d30aa807ed
bmp-dot <bmpeterson@gmail.com>
2025-04-01 21:52:22 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CalculatorApp from './App'; // Make sure App.jsx is in the same folder
+import './index.css';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <CalculatorApp />
+  </React.StrictMode>
+);
